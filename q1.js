1c3058738bbe9c732e90b0ef65aa3cd70d4cd356
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-17 18:09:33 +0000

    #

---
+++ q1.js
@@ -0,0 +1,19 @@
+//Conditional statements are programming constructs that allow the execution of different code blocks based on whether a specified condition evaluates to true or false. In most programming languages, the basic syntax for conditional statements includes if, else if (optional), and else (optional) blocks.
+
+if (condition1) {
+    // Code to be executed if condition1 is true
+} else if (condition2) {
+    // Code to be executed if condition2 is true
+} else {
+    // Code to be executed if none of the conditions are true
+}
+
+let num = 0;
+
+if (num > 0) {
+    console.log("The number is positive.");
+} else if (num < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
