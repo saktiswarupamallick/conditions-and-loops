353c141ef333178bb67a4402b451add842ee461d
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-17 18:21:07 +0000

    #

---
+++ q5.js
@@ -0,0 +1,17 @@
+// Ascending order using a while loop
+console.log("Ascending order:");
+
+let ascendingCounter = 1;
+while (ascendingCounter <= 25) {
+  console.log(ascendingCounter);
+  ascendingCounter++;
+}
+
+// Descending order using a while loop
+console.log("\nDescending order:");
+
+let descendingCounter = 25;
+while (descendingCounter >= 1) {
+  console.log(descendingCounter);
+  descendingCounter--;
+}
