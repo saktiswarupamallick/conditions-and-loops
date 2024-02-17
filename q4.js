625952fdd9184984e393b931fca5fd8faa9d9627
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-17 18:20:16 +0000

    #

---
+++ q4.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 25;
+
+console.log("Output:");
+
+// Using a for loop to generate numbers between num1 and num2
+for (let i = num1; i <= num2; i++) {
+  console.log(i);
+}
